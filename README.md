2b36268b1b1ec753f03cab5ac44b00f9c8b95f88
Anmeet Sekhon <113476064+AnmeetS@users.noreply.github.com>
2024-03-19 03:15:09 -0400

    Motor Control
    Created a pdf describing logic as well as a code snippet for synced control of motors.
    
    Next goal: Read XY coordinates from touchpad and map to motor angles

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Repository for the all the code related to my Ball Balancing Robot.
 I'm currently working on it, this readme will be updated soon!
 ## Updates:
-I just worked on a python calculator that lets me visualize the model eventually allowing me to text out the inverse kinematics
+- March 17, 2024: Worked on the motor control logic. More info found in the PDF
+- March 10, 2024: I just worked on a python calculator that lets me visualize the model eventually allowing me to text out the inverse kinematics
---
+++ Synced_Motor_Control/Synced_Motor_Control.ino
@@ -0,0 +1,85 @@
+//Libraries
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include <math.h>
+
+//Step Constants
+#define oneStep 2
+#define twoStep 3
+#define threeStep 4
+
+//Direction Constants
+#define oneDir 5
+#define twoDir 6
+#define threeDir 7
+
+//Setting up the classes
+AccelStepper stepper1(1, oneStep, oneDir);
+AccelStepper stepper2(1, twoStep, twoDir);
+AccelStepper stepper3(1, threeStep, threeDir);
+MultiStepper steppers;
+
+double angToStep = 0.556;
+long tarAngle[3]={ 45, 45, 45 };
+long posTar[3]={ 0, 0, 0};
+double speed[3] = { 0, 0, 0 }, speedPrev[3], ks = 1; 
+long posCur[3]={ 0,0,0 };
+long posOrig[3]={-90, 180, 90};
+
+void setup() {
+  // Adding the steppers to the steppersControl instance for multi stepper control
+  Serial.begin(115200);
+  // Adding the steppers to the steppersControl instance for multi stepper control
+  steppers.addStepper(stepper1);
+  steppers.addStepper(stepper2);
+  steppers.addStepper(stepper3);
+
+  for (int i = 0; i < 3; i++){
+    posOrig[i]=posOrig[i]*angToStep;
+  }
+
+  stepper1.setCurrentPosition(posOrig[0]);
+  stepper2.setCurrentPosition(posOrig[1]);
+  stepper3.setCurrentPosition(posOrig[2]);
+  
+  Serial.println((String) "pos 1 "+stepper1.currentPosition()+"pos2 "+stepper2.currentPosition() +"pos3 "+ stepper3.currentPosition());
+
+  delay(5000); 
+
+  stepper1.setMaxSpeed(speed[0]);
+  stepper2.setMaxSpeed(speed[1]);
+  stepper3.setMaxSpeed(speed[2]);  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  moveTo(); 
+}
+
+void moveTo(){
+  
+  for (int i = 0; i < 3; i++) {
+      speedPrev[i] = speed[i];    
+      posTar[i]  = tarAngle[i] * angToStep;                                                                                        //sets previous speed
+      posCur[i] = (i == 0) * stepper1.currentPosition() + (i == 1) * stepper2.currentPosition() + (i == 2) * stepper3.currentPosition();  //sets current position
+      speed[i] = abs(posCur[i] - posTar[i]) * ks;                                                                                            //calculates the error in the current position and target position
+      speed[i] = constrain(speed[i], 0, 1000); //constrains sped from 0 to 1000                                                                                   
+    }
+  stepper1.setMaxSpeed(speed[0]);
+  stepper2.setMaxSpeed(speed[1]);
+  stepper3.setMaxSpeed(speed[2]);
+  //sets acceleration to be proportional to speed
+  stepper1.setAcceleration(speed[0]*1);
+  stepper2.setAcceleration(speed[0]*1);
+  stepper3.setAcceleration(speed[0]*1);
+  //sets target positions
+  stepper1.moveTo(posTar[0]);
+  stepper2.moveTo(posTar[1]);
+  stepper3.moveTo(posTar[2]);
+  //runs stepper to target position (increments at most 1 step per call)
+  stepper1.run();
+  stepper2.run();
+  stepper3.run();
+  
+}
