94f40abe4557c23042807afac5aef6fd9d826428
Anmeet Sekhon <113476064+AnmeetS@users.noreply.github.com>
2024-03-20 01:46:39 -0400

    Create TouchScreenValue.ino

---
+++ TouchScreenValue/TouchScreenValue.ino
@@ -0,0 +1,19 @@
+#include <TouchScreen.h>
+
+TouchScreen ts = TouchScreen(A1, A0, A3, A2, 0 ); //touch screen pins (XGND, YGND, X5V, Y5V)
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  PID(0, 0);  //(X setpoint, Y setpoint) -- must be looped
+
+}
+
+void PID(double xSetPoint, double ySetPoint){
+  TSPoint p = ts.getPoint();  //measure X and Y positions
+  Serial.println((String) "X OUT = " + (p.x - xSetPoint) + "   Y OUT = " + (p.y - ySetPoint));  //print X and Y outputs
+}
