7b5a508cc270756c84ae2ae93d49a055fa876017
Anmeet Sekhon <113476064+AnmeetS@users.noreply.github.com>
2024-04-17 19:03:31 -0400

    Update TouchScreenValue.ino

--- TouchScreenValue/TouchScreenValue.ino
+++ TouchScreenValue/TouchScreenValue.ino
@@ -10,10 +10,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   PID(0, 0);  //(X setpoint, Y setpoint) -- must be looped
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 }
 
 void PID(double xSetPoint, double ySetPoint){
